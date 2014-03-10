62a256add5074f93d848677e5f64f33a7513cb5c
Francisco Baio Dias <xicombd@gmail.com>
2014-03-10 10:31:59 +0000

    person class

--- main.cpp
+++ main.cpp
@@ -5,11 +5,31 @@
 
 using namespace std;
 
+class Person {
+    int id;
+    vector<int> friends;
+  public:
+    Person (int);
+    int getId () {return id;}
+    void newFriend (int i) {friends.push_back(i);}
+};
+
+Person::Person (int i) {
+  id = i;
+}
+
+vector<int> u,v;
+vector<Person*> people;
+int n,p;
+
+
 int main () {
-  vector<int> u,v;
-  int n,p;
 
   scanf("%u %u", &n,&p);
+
+  for(int i=0;i<n;i++) {
+    people.push_back(new Person(i));
+  }
 
   int pu, pv;
 
@@ -18,6 +38,16 @@ int main () {
 
     u.push_back(pu);
     v.push_back(pv);
+  }
+
+
+  // DEBUG
+  for(int i=0;i<n;i++) {
+    cout << "Person " << people[i]->getId() << "\n";
+  }
+
+  for(int i=0;i<p;i++) {
+    cout << "Partilha " << u[i] << " " << v[i] << "\n";
   }
 
   return 0;

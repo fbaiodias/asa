8aa94b6698fc759711fcd8bd01269404e9bfaf06
Francisco Baio Dias <xicombd@gmail.com>
2014-03-14 16:22:27 +0000

    stuuuuf

--- main.cpp
+++ main.cpp
@@ -7,18 +7,23 @@ using namespace std;
 
 class Person {
     int id;
+    int d, low;
     vector<int> friends;
   public:
-    Person (int i) {id = i;};
+    Person (int i) {id = i; d=-1;};
     int getId () {return id;}
     void newFriend (int i) {friends.push_back(i);}
     int getFriends(){return friends.size();}
     int getFriend(int i){return friends[i];}
-
+    bool unvisited() {return (d == -1);}
+    void visit(visited) {d=low=visited;}
 };
 
 vector<Person*> people;
+vector<Person*> stack;
 int n,p;
+int visited = 0, L = 0;
+
 
 
 int main () {
@@ -30,13 +35,17 @@ int main () {
   }
 
   int pu, pv;
-
   for(int i=0;i<p;i++) {
     scanf("%u %u", &pu, &pv);
 
     people[pu-1]->newFriend(pv);
   }
 
+  for(int i=0;i<n;i++) {
+    if(people[i]->unvisited()){
+      tarjanVisit(i);
+    }
+  }
 
   // DEBUG
   for(int i=0;i<n;i++) {
@@ -48,4 +57,22 @@ int main () {
   }
 
   return 0;
+}
+
+void tarjanVisit(int i) {
+  people[i]->visit();
+
+  visited = visited + 1
+  Push(L, u);
+  for each v ∈Adj[u]
+  do if (d[v] = ∞ || v ∈ L)
+  // Ignora vértices de SCCs já identiﬁcados
+  then if d[v] = ∞
+  then Tarjan_Visit(v)
+  low[u] = min(low[u], low[v])
+  if d[u] = low[u] ✄ Raiz do SCC
+  then repeat
+  v = Pop(L)
+  // Vértices retirados deﬁnem SCC
+until u = v
 }

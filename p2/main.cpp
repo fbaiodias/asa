d3c1f7939a66d3c844da9919e4fbb017d8e8a8ae
velkan14 <velkan14@gmail.com>
2014-04-21 00:45:53 +0100

    Algoritmo

--- p2/main.cpp
+++ p2/main.cpp
@@ -2,17 +2,28 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 /*----------- Classes ------------*/
 class Point {
     vector<int> connects;
+    int color; // 0 is white; 1 is black; 2 is gray;
+    int time;
+    int father;
   public:
     Point () {};
-    void newConnect (int i) {connects.push_back(i);}
+    void newConnect (int i) {connects.push_back(i); color = 0; time = -1; father = -1;}
     int getConnectsSize(){return connects.size();}
     int getConnect(int i){return connects[i];}
+    
+    int getColor(){return color;}
+    int getTime(){return time;}
+    int getFather(){return father;}
+    void setColor(int c){color = c;}
+    void setFather(int i){father = i;}
+    void setTime(int i){time = i;}
 };
 
 class Problem {
@@ -22,6 +33,7 @@ class Problem {
     void newPoint (int i) {points.push_back(i);}
     int getPointsSize(){return points.size();}
     int getPoint(int i){return points[i];}
+
 };
 
 /*----------- Global Variables ------------*/
@@ -29,9 +41,48 @@ vector<Point*> points;
 vector<Problem*> problems;
 int n,m,h;
 int garbage = 0;
-
+queue<int> stack;
 /*----------- Functions ------------*/
+  void BFS(int s){
+  	points[s]->setColor(2);
+  	points[s]->setTime(0);
+  	stack.push(s);
+  	while (!stack.empty()){
+  		int u = stack.front();
+  		stack.pop();
+  		for(int i = 0; i <= points[i]->getConnectsSize(); i++){
+  			if(points[points[u]->getConnect(i)]->getColor()==0){
+  				points[points[u]->getConnect(i)]->setColor(2);
+  				points[points[u]->getConnect(i)]->setTime(points[u]->getTime()+1);
+  				points[points[u]->getConnect(i)]->setFather(u);
+  				stack.push(points[u]->getConnect(i));
+  			}
+  		}
+  		points[u]->setColor(1);
+  	}
+  }
+  std::vector<int> pathFinder(int s, int t){
+  	std::vector<int> path;
+  	int j=0;
+  	BFS(s);
+  	u=t;
+  	while(u!=s){
+	  	for(int i=0;i <= points[u]->getConnectsSize; i++){
+	  		if (time > points[points[u]->getConnect[i]]->getTime()){
+	  			path[j] = points[u]->getConnect[i];
+	  		}
+	  	}
+	  	u=path[j++];
+    }
+    return path;
 
+  }
+  void FordFulkerson(int s,int t){
+  	std::vector<int> path = pathFinder(s,t);
+  	while(points[t]->getTime()!=-1){
+
+  	}
+  }
 
 /*----------- Main ------------*/
 int main () {
@@ -81,7 +132,12 @@ int main () {
     cout << "\n\n";
   }
 
-  // OUTPUT
 
+  // OUTPUT
+  BFS(0);
+  for (int i =0; i<5; i++){
+  	printf("%i: %i\n", i, points[i]->getFather());
+  	printf("   %i\n", points[i]->getTime());
+  }
   return 0;
 }
